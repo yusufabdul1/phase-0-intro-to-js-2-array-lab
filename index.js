2a47a294fadce822ea5aebfaf686c101d9099fc3
Yusuf Abdul <yusufabdul212112@gmail.com>
2024-10-09 07:09:39 +0300

    adds new file

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.splice(2, 1)
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.splice(0, 1)
+}
+
+function appendCat(){
+    return [...cats, "Broom"]
+}
+
+function prependCat(){
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
 // Write your solution here!
